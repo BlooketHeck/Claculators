6aa090af8a37f3cb3379d0feb760147663a3e78e
BlooketHeck <94115787+BlooketHeck@users.noreply.github.com>
2022-11-14 16:22:39 -0500

    Create Subtractor.py

---
+++ Subtractor.py
@@ -0,0 +1,6 @@
+# If you would like to use the subtractor go to https://www.programming-hero.com/code-playground/python/index.html and paste the code.
+import math
+
+x = float(input("Input the first number you'd like to subtract:"))
+y = float(input("Input the second number that you would like to subtract:"))
+print(x - y)

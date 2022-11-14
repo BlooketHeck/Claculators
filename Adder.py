0368bc821a8677c7e13a521b2783c1f16bc2f9a6
BlooketHeck <94115787+BlooketHeck@users.noreply.github.com>
2022-11-14 16:21:22 -0500

    Create Adder.py

---
+++ Adder.py
@@ -0,0 +1,6 @@
+# If you would like to use the adder go to https://www.programming-hero.com/code-playground/python/index.html and paste the code.
+import math
+
+x = float(input("Input the first number you'd like to add:"))
+y = float(input("Input the second number that you would like to add:"))
+print(x + y)

3019cdf4ff2bfe83d48a0d51b1130d54009a9093
BlooketHeck <94115787+BlooketHeck@users.noreply.github.com>
2022-11-14 17:12:59 -0500

    Create Square Root Finder.py

---
+++ Square Root Finder.py
@@ -0,0 +1,7 @@
+# You can NOT use this square root finder in a python playground if you wanna know why check README.md
+# To use this square root finder create a repl on Replit (I'll add more ways to use this as soon as I find them)
+from math import isqrt
+
+x = isqrt("Input the square root you want to find")
+
+print(x)

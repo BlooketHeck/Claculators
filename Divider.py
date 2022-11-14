46db8b0141112fb191d60f82cba0a2aa545257f3
BlooketHeck <94115787+BlooketHeck@users.noreply.github.com>
2022-11-14 16:15:32 -0500

    Update Divider.py

--- Divider.py
+++ Divider.py
@@ -1,3 +1,4 @@
+# If you would like to use the divider go to https://www.programming-hero.com/code-playground/python/index.html and paste the code.
 import math
 
 x = float(input("Input the first number you'd like to divide:"))

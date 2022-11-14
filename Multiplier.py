099b2be4e8ad6e716d63a088ea9fa5a5db84cb47
BlooketHeck <94115787+BlooketHeck@users.noreply.github.com>
2022-11-14 16:17:58 -0500

    Create Multiplier.py

---
+++ Multiplier.py
@@ -0,0 +1,6 @@
+# If you would like to use the multiplier go to https://www.programming-hero.com/code-playground/python/index.html and paste the code.
+import math
+
+x = float(input("Input the first number you'd like to multiply:"))
+y = float(input("Input the second number that you would like to multiply:"))
+print(x * y)

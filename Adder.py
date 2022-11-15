9beb5819c22f5de12a45cd9eee78090ebd924cea
BlooketHeck <94115787+BlooketHeck@users.noreply.github.com>
2022-11-15 12:29:23 -0500

    Update Adder.py

--- Adder.py
+++ Adder.py
@@ -1,6 +1,9 @@
-# If you would like to use the adder go to https://www.programming-hero.com/code-playground/python/index.html and paste the code.
+# If you would like to use the adder go to https://www.programming-hero.com/code-playground/python/index.html and paste the code for num 3-5 leave 0 if blank.
 import math
 
-x = float(input("Input the first number you'd like to add:"))
-y = float(input("Input the second number that you would like to add:"))
-print(x + y)
+num1 = float(input("Input the first number you'd like to add:"))
+num2 = float(input("Input the second number that you would like to add:"))
+num3 = float(input("Input the third number that you would like to add:"))
+num4 = float(input("Input the forth number that you would like to add:"))
+num5 = float(input("Input the fith number that you would like to add:"))
+print(num1 + num2 + num3 + num4 + num5)
